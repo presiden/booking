130eea5bd6daf23ffc05eb5e63ae96f74dd1444f
Prasidyo <prasidyo@gmail.com>
2025-05-26 23:21:42 +0700

    Update FilmController.java, FilmService.java, and FilmServiceImpl.java

--- src/main/java/com/okestudio/booking/service/FilmService.java
+++ src/main/java/com/okestudio/booking/service/FilmService.java
@@ -1,13 +1,23 @@
 package com.okestudio.booking.service;
 
 import com.okestudio.booking.dto.FilmRequestDto;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.okestudio.booking.dto.FilmDetailResponseDto;
-import com.okestudio.booking.dto.FilmSummaryResponseDto;
+import com.okestudio.booking.dto.FilmResponseDto;
 import com.okestudio.booking.dto.ResultPageResponseDto;
+import com.okestudio.booking.view.AvailableFilmView;
 
 public interface FilmService {
 
-    public ResultPageResponseDto<FilmSummaryResponseDto> getFilms(Integer page, Integer size, String sortBy, String sortDirection, FilmRequestDto dto);
+    public ResultPageResponseDto<FilmResponseDto> getFilms(Integer page, Integer size, String sortBy, String sortDirection, FilmRequestDto dto);
 
     public FilmDetailResponseDto getFilmById(Long id);
+
+    ResultPageResponseDto<AvailableFilmView> getAll(Pageable pageable);
+
+    ResultPageResponseDto<AvailableFilmView> getByTitleContaining(String title, Pageable pageable);
+
 }
--- src/main/java/com/okestudio/booking/service/impl/FilmServiceImpl.java
+++ src/main/java/com/okestudio/booking/service/impl/FilmServiceImpl.java
@@ -1,5 +1,6 @@
 package com.okestudio.booking.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -10,14 +11,17 @@ import org.springframework.stereotype.Service;
 
 import com.okestudio.booking.dto.FilmRequestDto;
 import com.okestudio.booking.dto.FilmDetailResponseDto;
-import com.okestudio.booking.dto.FilmSummaryResponseDto;
+import com.okestudio.booking.dto.FilmResponseDto;
 import com.okestudio.booking.dto.ResultPageResponseDto;
 import com.okestudio.booking.entity.Film;
+import com.okestudio.booking.mapper.AvailableFilmMapper;
 import com.okestudio.booking.mapper.FilmMapper;
+import com.okestudio.booking.repository.AvailableFilmViewRepository;
 import com.okestudio.booking.repository.FilmRepository;
 import com.okestudio.booking.repository.GenreRepository;
 import com.okestudio.booking.service.FilmService;
 import com.okestudio.booking.specification.FilmSpecification;
+import com.okestudio.booking.view.AvailableFilmView;
 import com.okestudio.util.PaginationUtil;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -27,18 +31,25 @@ public class FilmServiceImpl implements FilmService {
 
     private final FilmRepository filmRepository;
     private final GenreRepository genreRepository;
+    private final AvailableFilmViewRepository availableFilmViewRepository;
+    private final FilmMapper filmMapper;
 
-    public FilmServiceImpl(FilmRepository filmRepository, GenreRepository genreRepository) {
+    public FilmServiceImpl(FilmRepository filmRepository, GenreRepository genreRepository,
+            AvailableFilmViewRepository availableFilmViewRepository, FilmMapper filmMapper) {
         this.filmRepository = filmRepository;
         this.genreRepository = genreRepository;
+        this.availableFilmViewRepository = availableFilmViewRepository;
+        this.filmMapper = filmMapper;
     }
 
     @Override
-    public ResultPageResponseDto<FilmSummaryResponseDto> getFilms(Integer page, Integer size, String sortBy, String sortDirection,
+    public ResultPageResponseDto<FilmResponseDto> getFilms(Integer page, Integer size, String sortBy,
+            String sortDirection,
             FilmRequestDto dto) {
         Pageable pageable = PaginationUtil.getPageable(page, size, sortBy, sortDirection);
         Page<Film> films = filmRepository.findAll(FilmSpecification.filter(dto), pageable);
-        List<FilmSummaryResponseDto> filmResponseDtos = FilmMapper.INSTANCE.toSummaryDtos(films.getContent());
+        List<FilmResponseDto> filmResponseDtos = new ArrayList<>();// =
+                                                                   // FilmMapper.INSTANCE.toSummaryDtos(films.getContent());
 
         return new ResultPageResponseDto<>(filmResponseDtos, films.getTotalElements(), films.getTotalPages(),
                 films.getSize(), films.getNumber());
@@ -46,14 +57,27 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public FilmDetailResponseDto getFilmById(Long id) {
-        Film film = findFilmOrThrow(id);
+        Film film = filmRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Film not found with id: " + id));
 
-        return FilmMapper.INSTANCE.toDto(film);
+        return filmMapper.toFilmDetailResponseDto(film);
     }
 
     private Film findFilmOrThrow(Long id) {
         return filmRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Film not found with id: " + id));
+    }
+
+    @Override
+    public ResultPageResponseDto<AvailableFilmView> getAll(Pageable pageable) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public ResultPageResponseDto<AvailableFilmView> getByTitleContaining(String title, Pageable pageable) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByTitleContaining'");
     }
 
 }
